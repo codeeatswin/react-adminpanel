77ce8f4e10ec70306da437e2f4e65175ea6ff5ba
codeeatswin <codeeats@gmail.com>
2023-08-30 01:14:54 +0900

    SideNav

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,10 +1,33 @@
-import { Box, Typography } from '@mui/material';
-
+import { Typography } from '@mui/material';
+import { Menu, MenuItem, Sidebar } from 'react-pro-sidebar';
+import DashboardOutlinedIcon from '@mui/icons-material/DashboardOutlined';
+import StyleOutlinedIcon from '@mui/icons-material/StyleOutlined';
+import SourceOutlinedIcon from '@mui/icons-material/SourceOutlined';
+import AnalyticsOutlinedIcon from '@mui/icons-material/AnalyticsOutlined';
+import { useTheme } from '@emotion/react';
 function SideNav() {
+  const theme = useTheme();
   return (
-    <Box>
-      <Typography>SideNav</Typography>
-    </Box>
+    <Sidebar
+      style={{ height: '100%', top: 'auto' }}
+      breakPoint='md'
+      backgroundColor={theme.palette.neutral.light}
+    >
+      <Menu>
+        <MenuItem active icon={<DashboardOutlinedIcon />}>
+          <Typography variant='body2'> Dashboard</Typography>
+        </MenuItem>
+        <MenuItem active icon={<SourceOutlinedIcon />}>
+          <Typography variant='body2'> Content</Typography>
+        </MenuItem>
+        <MenuItem active icon={<AnalyticsOutlinedIcon />}>
+          <Typography variant='body2'> Analytics</Typography>
+        </MenuItem>
+        <MenuItem active icon={<StyleOutlinedIcon />}>
+          <Typography variant='body2'> Customization</Typography>
+        </MenuItem>
+      </Menu>
+    </Sidebar>
   );
 }
 
